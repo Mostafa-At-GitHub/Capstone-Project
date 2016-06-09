6c3dcda8fde37aa5b4a81e335366ac6752e1c8ce
Sarah Ehab Mostafa <sarahehabm@gmail.com>
2016-06-09 18:33:52 +0200

    Add missing file

---
+++ CarbCalculator/app/src/main/java/com/sarahehabm/carbcalculator/common/Preferences.java
@@ -0,0 +1,36 @@
+package com.sarahehabm.carbcalculator.common;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ Created by Sarah E. Mostafa on 09-Jun-16.
+ */
+
+public class Preferences {
+    private final static String PREFERENCES_NAME = "CarbCalculatorPreferences";
+    public final static String KEY_NAME = "name";
+    public final static String KEY_IMAGE_URL = "image_url";
+
+    private Context context;
+    private SharedPreferences sharedPreferences;
+
+    public Preferences(Context context) {
+        this.context = context;
+        if(sharedPreferences == null) {
+            sharedPreferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+        }
+    }
+
+    public boolean putString(String key, String value) {
+        return sharedPreferences.edit().putString(key, value).commit();
+    }
+
+    public String getString(String key) {
+        return sharedPreferences.getString(key, null);
+    }
+
+    public String getString(String key, String defaultValue) {
+        return sharedPreferences.getString(key, defaultValue);
+    }
+}
