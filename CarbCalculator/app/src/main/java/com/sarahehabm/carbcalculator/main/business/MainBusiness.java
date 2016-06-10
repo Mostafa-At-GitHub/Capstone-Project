a94fb9e2047bbc965f3eab31bcc152c40bfcca19
Sarah Ehab Mostafa <sarahehabm@gmail.com>
2016-06-10 14:05:28 +0200

    Modify MainBusiness

--- CarbCalculator/app/src/main/java/com/sarahehabm/carbcalculator/main/business/MainBusiness.java
+++ CarbCalculator/app/src/main/java/com/sarahehabm/carbcalculator/main/business/MainBusiness.java
@@ -3,11 +3,13 @@ package com.sarahehabm.carbcalculator.main.business;
 import android.content.Context;
 import android.support.v4.content.CursorLoader;
 
-import com.sarahehabm.carbcalculator.common.database.CarbCounterContract.*;
+import com.sarahehabm.carbcalculator.common.database.CarbCounterContract.MealEntry;
 import com.sarahehabm.carbcalculator.common.database.CarbCounterInterface;
 import com.sarahehabm.carbcalculator.common.model.Meal;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Sarah E. Mostafa on 11-May-16.
@@ -23,7 +25,44 @@ public class MainBusiness {
     }
 */
     public static ArrayList<Meal> getTodayMeals(Context context) {
-        return CarbCounterInterface.getAllMeals(context);
+        ArrayList<Meal> allMeals = CarbCounterInterface.getAllMeals(context);
+
+        if(allMeals == null || allMeals.isEmpty())
+            return allMeals;
+
+        Calendar calendarStart = Calendar.getInstance(), calendarEnd = Calendar.getInstance();
+//        calendarStart.setTimeInMillis(System.currentTimeMillis());
+
+        calendarStart.set(Calendar.HOUR_OF_DAY, 0);
+        calendarStart.set(Calendar.MINUTE, 0);
+        calendarStart.set(Calendar.SECOND, 0);
+        calendarStart.set(Calendar.MILLISECOND, 0);
+
+//        calendarEnd = calendarStart;
+//        calendarEnd.add(Calendar.MILLISECOND, 86400000);
+        calendarEnd.set(Calendar.HOUR_OF_DAY, 23);
+        calendarEnd.set(Calendar.MINUTE, 59);
+        calendarEnd.set(Calendar.SECOND, 59);
+        calendarEnd.set(Calendar.MILLISECOND, 999);
+
+//        long timestampStart = calendarStart.getTimeInMillis(),
+//                timestampEnd = calendarEnd.getTimeInMillis();
+
+        Date dateStart = new Date(calendarStart.getTimeInMillis()),
+                dateEnd = new Date(calendarEnd.getTimeInMillis());
+
+        ArrayList<Meal> todayMeals = new ArrayList<>();
+        for (int i = 0; i < allMeals.size(); i++) {
+            Meal meal = allMeals.get(i);
+            long mealTimestamp = meal.getTimestamp();
+            Date mealDate = new Date(mealTimestamp);
+
+
+            if(mealDate.after(dateStart) && mealDate.before(dateEnd))
+                todayMeals.add(meal);
+        }
+
+        return todayMeals;
     }
 
     public static CursorLoader getCursorLoader(Context context) {
