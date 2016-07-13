6de68fb2b94adcf9747cf4ddab69b6bb86640326
Sarah Ehab Mostafa <sarahehabm@gmail.com>
2016-07-13 21:05:23 +0200

    Remove unused library

--- CarbCalculator/steppers/src/main/java/me/drozdzynski/library/steppers/OnItemClickAction.java
+++
@@ -1,9 +0,0 @@
-package me.drozdzynski.library.steppers;
-
-/**
- Created by Sarah E. Mostafa on 03-Jun-16.
- */
-
-public interface OnItemClickAction {
-    void onItemClick(int position, int id);
-}
